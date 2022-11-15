dc95164ec50732da956782d73d8bbe1c45454084
1parakeet1 <95216234+1parakeet1@users.noreply.github.com>
2022-11-15 17:02:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.position(525, 200);
+}
+
+function draw(){
+
+}
+
+
+function take_snapshot(){
+    save('image.png');
+}
